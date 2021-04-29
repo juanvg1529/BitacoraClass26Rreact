60236ad55f458d8b6658df5c2c4dca5039165847
juanvg1529 <67924398+juanvg1529@users.noreply.github.com>
2021-04-29 17:44:42 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import React from 'react';
+export default function App() {
+
+return (
+        <div >
+        <h1 > Soy un componente de react </h1> 
+        <button >Push Me </button> 
+        </div >
+    );
+}
